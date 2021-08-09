08212d4b5ca9e27232737c3d26aa9b0ed45b8edb
thinkemm <yz15256289025@163.com>
2021-08-09 14:43:47 +0800

    线程池修改

--- src/main/java/TestJava.java
+++ src/main/java/TestJava.java
@@ -1,6 +1,18 @@
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TestJava {
     public static void main(String[] args) {
         System.out.println("123");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,4,1000L, TimeUnit.SECONDS,new LinkedBlockingDeque<>());
+
+        ReentrantLock r = new ReentrantLock();
+        r.lock();
+
+
     }
 }
 
