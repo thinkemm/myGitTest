95e948c891d930332af81e291638a164999e10c8
thinkemm <yz15256289025@163.com>
2021-08-22 22:36:03 +0800

    mybatisplus自动填充 1、使用注解@TableFile 2、创建metaObjectHandler实现类

--- SpringbootDemo/src/main/java/com/mybatisplus/springbootdemo/pojo/User.java
+++ SpringbootDemo/src/main/java/com/mybatisplus/springbootdemo/pojo/User.java
@@ -1,10 +1,14 @@
 package com.mybatisplus.springbootdemo.pojo;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 /**
  * @Author Emm
@@ -20,11 +24,16 @@ public class User {
      * auto自增——数据库id必须自增
      * uuid——生成uuid（string类型）
      * id_work——生成唯一id（Long类型）
-     * input——手动创建id(如果是自增可以不输入，如果不是自增必须输入不然会报错)
+     * input——手动创建id(如果是自增可以不输入，如果不是自增，必须输入不然会报错)
      */
-    @TableId(type = IdType.INPUT)
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
     private int age;
     private String email;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModify;
 }
